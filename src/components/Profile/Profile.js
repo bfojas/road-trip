5e0e50c5db495c87f4dca36cecfbd4dcac05b11c
Logan Marshall <wildmovementco@gmail.com>
2019-02-07 20:17:41 -0700

    styled edit cover image button

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,21 +6,43 @@ import TripsList from "./TripsList";
 import Following from "./Following";
 import SavedTrips from "./SavedTrips";
 import avatar from "../../images/batman.png";
-import defaultCover from "../../images/default-cover.jpg"
+import defaultCover from "../../images/default-cover.jpg";
+import mountain from "../../images/mountain-road.jpg";
 import "./Profile.scss";
 
 class Profile extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCoverEdit: false
+        }
+    }
+
+    toggle = () => {
+        let coverState = this.state.showCoverEdit;
+        this.setState({
+            showCoverEdit: !coverState
+        })
+    }
+
     render() {
+        const { showCoverEdit } = this.state;
         const { match, user } = this.props;
         const profileImage = user ? user.profile_image || avatar : avatar;
         const coverImage = user ? user.cover_image || defaultCover : defaultCover;
+        const coverEditStyle = showCoverEdit ? { display: "flex" } : { display: "none" };
+        const coverIconStyle = showCoverEdit ? { fontSize: "18px"} : { fontSize: "24px"};
 
         return user ? (
             <div className="profile-container">
-                <div className="profile-hero" style={{backgroundImage: `url(${coverImage})`}}>
-                    <i className="fas fa-camera cover-edit-icon"></i>
-                    <div className="cover-edit-box">
+                <div className="profile-hero" 
+                    style={{backgroundImage: `url(${coverImage})`}}
+                    onMouseEnter={this.toggle}
+                    onMouseLeave={this.toggle}
+                >
+                    <i className="fas fa-camera cover-edit-icon" style={coverIconStyle}></i>
+                    <div className="cover-edit-box" style={coverEditStyle}>
                         <span>EDIT COVER PHOTO</span>
                     </div>
                     <div className="profile-info">
@@ -30,7 +52,6 @@ class Profile extends Component {
                                 <span>UPDATE</span>
                             </div>
                         </div>
-                        {/* <img src={user.profile_image || avatar} /> */}
                         <h2>{user.name}</h2>
                         <span>{user.bio || "Short bio goes here."}</span>
                     </div>
